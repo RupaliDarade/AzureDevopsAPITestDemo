22264dc68e6be2dce9a45ec40c88980babb26d5f
RupaliDarade <71090094+RupaliDarade@users.noreply.github.com>
2020-11-03 05:33:39 +0100

    Add files via upload

---
+++ test/IOT_POC.bat
@@ -0,0 +1,7 @@
+@echo on
+
+cd %Tricentis_home%
+
+cd..
+
+tcshell -workspace "C:\Tosca_Projects\Tosca_Workspaces\Azure_CI\Azure_CI.tws" -login "Admin" "" "C:\Users\DKSORDE\OneDrive - Alfa Laval\Desktop\Tosca Azure\IOT_POC.tcs"
